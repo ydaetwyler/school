8ae4973e8d5a5220e736f42f14dc9f220203bc01
ydaetwyler <ydaetwyler@previon.ch>
2021-06-20 17:23:07 +0200

    reset for further chapters

--- pro-mern-stack-2/server/server.js
+++ pro-mern-stack-2/server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.use('/public/', express.static('public'));
+app.use(express.static('public'));
 
 app.listen(3000, () => {
     console.log('App is running on Port 3000');
