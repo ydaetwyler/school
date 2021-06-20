9a943d43569d8e6abf3af833f229896650a2545c
ydaetwyler <ydaetwyler@previon.ch>
2021-06-20 17:21:51 +0200

    Excercise: Express 2

---
+++ pro-mern-stack-2/server/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.use('/public/', express.static('public'));
+
+app.listen(3000, () => {
+    console.log('App is running on Port 3000');
+});
