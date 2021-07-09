d222ce3929a223c8f3db555c7761633e9a527706
ydaetwyler <ydaetwyler@previon.ch>
2021-07-10 00:18:49 +0200

    Solved <br /> problem with array.map

--- pro-mern-stack-2/public/app.js
+++ pro-mern-stack-2/public/app.js
@@ -4,9 +4,10 @@ var continents = ['Mittelerde', 'Pangea', 'Westeros', 'Hoth'];
 var helloContinents = Array.from(continents, function (c) {
   return "Hello ".concat(c, "!");
 });
-var message = helloContinents.join('<br />');
 var element = /*#__PURE__*/React.createElement("div", {
   title: "Outer div"
-}, /*#__PURE__*/React.createElement("h1", null, message));
+}, helloContinents.map(function (continent) {
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, continent), /*#__PURE__*/React.createElement("br", null));
+}));
 var toRender = document.getElementById('content');
 ReactDOM.render(element, toRender);
--- pro-mern-stack-2/src/app.jsx
+++ pro-mern-stack-2/src/app.jsx
@@ -1,10 +1,14 @@
 const continents = ['Mittelerde', 'Pangea', 'Westeros', 'Hoth'];
 const helloContinents = Array.from(continents, c => `Hello ${c}!`);
-const message = helloContinents.join('<br />'); // It's not working
 
 const element = (
     <div title="Outer div">
-        <h1>{message}</h1>
+        {helloContinents.map(continent => (
+            <div>
+                <h1>{continent}</h1>
+                <br />
+            </div>
+        ))}
     </div>
 );
 const toRender = document.getElementById('content');
