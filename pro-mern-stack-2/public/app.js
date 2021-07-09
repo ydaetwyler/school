1d9f513f53df43242a30b76d29d62b47caa9adc7
ydaetwyler <ydaetwyler@previon.ch>
2021-07-10 00:13:02 +0200

    Tried use of <br /> in app.jsx join

--- pro-mern-stack-2/public/app.js
+++ pro-mern-stack-2/public/app.js
@@ -4,7 +4,7 @@ var continents = ['Mittelerde', 'Pangea', 'Westeros', 'Hoth'];
 var helloContinents = Array.from(continents, function (c) {
   return "Hello ".concat(c, "!");
 });
-var message = helloContinents.join(' ');
+var message = helloContinents.join('<br />');
 var element = /*#__PURE__*/React.createElement("div", {
   title: "Outer div"
 }, /*#__PURE__*/React.createElement("h1", null, message));
--- pro-mern-stack-2/src/app.jsx
+++ pro-mern-stack-2/src/app.jsx
@@ -1,6 +1,6 @@
 const continents = ['Mittelerde', 'Pangea', 'Westeros', 'Hoth'];
 const helloContinents = Array.from(continents, c => `Hello ${c}!`);
-const message = helloContinents.join(' ');
+const message = helloContinents.join('<br />'); // It's not working
 
 const element = (
     <div title="Outer div">
