83d6d5ddb173189fed413865b9d178872ef617fd
ydaetwyler <ydaetwyler@previon.ch>
2021-07-10 15:07:53 +0200

    Worked on expressapp, not finished

--- excercises_unterricht/expressapp_node16/server/server.mjs
+++ excercises_unterricht/expressapp_node16/server/server.mjs
@@ -3,7 +3,13 @@ import express from 'express'
 const app = express()
 const PORT = 3000
 
-app.use(express.json())
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+app.get("/users", (req, res) => {
+    res.sendFile(path.join(__dirname, ".(users.html"))
+})
+
+app.use()
 
 app.listen(PORT, () => {
     console.log(`App started, listening at ${PORT}`)
