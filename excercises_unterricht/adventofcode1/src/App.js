c4bec87cdd4e1fed03663fba629eac224fc08e3a
ydaetwyler <ydaetwyler@previon.ch>
2021-07-10 17:14:21 +0200

    adventofcode1 solved with react

--- excercises_unterricht/adventofcode1/src/App.js
+++ excercises_unterricht/adventofcode1/src/App.js
@@ -5,6 +5,7 @@ import axios from 'axios'
 const App = () => {
   const [numbers, setNumbers] = useState([])
   const [result, setResult] = useState([])
+  const [match, setMatch] = useState([])
 
 
   useEffect(() => {
@@ -12,23 +13,44 @@ const App = () => {
       .get('http://localhost:3001/allnumbers')
       .then(response => {
         setNumbers(response.data)
-        console.log(numbers);
       })
   }, [])
 
   useEffect(() => {
-    const result = numbers.filter(el => (el.number / 2020) = 0)
-    setResult(result)
-  }, numbers)
+    const checkSum = (numbers) => {
+      for (let i = 0; i < numbers.length; i++) {
+        for (let x = 0; x < numbers.length; x++) {
+          if (i !== x) {
+            const sum = numbers[i].number + numbers[x].number
+            if (sum === 2020) {
+              setMatch([numbers[i].number, numbers[x].number])
+              setResult([numbers[i].number * numbers[x].number])
+            }
+          }
+        }
+      }
+    }
+    checkSum(numbers)
+  }, [numbers])   
+
+  console.log(result)
 
   return (
     <div>
       <h1>AdventOfCode 1</h1>
-
+      <h2>These two numbers sum up to 2020</h2>
+      <ul>
+        {match.map(numb =>
+        <li key={numb}>
+          {numb}
+        </li>
+        )}
+      </ul>
+      <h2>These numbers multiplied are</h2>
       <ul>
         {result.map(res =>
-        <li key={res.id}>
-          {res.number}
+        <li key={res}>
+          {res}
         </li>
         )}
       </ul>
