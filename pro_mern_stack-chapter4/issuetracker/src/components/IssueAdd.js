bba5435e32ab8578a19563a0ac55d82a396ffb50
ydaetwyler <ydaetwyler@previon.ch>
2021-07-18 15:31:15 +0200

    Fresh start ;)

--- pro_mern_stack-chapter4/issuetracker/src/components/IssueAdd.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const IssueAdd = () => (
-    <div>
-        <p>Placeholder issue add.</p>
-    </div>
-)
-
-export default IssueAdd
--- pro_mern_stack-chapter4/issuetracker/src/components/IssueFilter.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const IssueFilter = () => (
-    <div>
-        <p>Placeholder issue filter.</p>
-    </div>
-)
-
-export default IssueFilter
