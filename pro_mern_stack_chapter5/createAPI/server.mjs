43b373510cbf8f56a431714e176cbb2f8e499e0d
ydaetwyler <ydaetwyler@previon.ch>
2021-08-26 22:51:25 +0200

    implemented validations

--- pro_mern_stack_chapter5/createAPI/server.mjs
+++ pro_mern_stack_chapter5/createAPI/server.mjs
@@ -1,5 +1,5 @@
 import express from 'express'
-import { ApolloServer } from 'apollo-server-express'
+import { ApolloServer, UserInputError } from 'apollo-server-express'
 import { GraphQLScalarType } from 'graphql'
 // Doesn't work with /language
 import { Kind } from 'graphql'
@@ -27,10 +27,14 @@ const GraphQLDate = new GraphQLScalarType({
         return value.toISOString();
     },
     parseValue(value) {
-        return new Date(value)
+        const dateValue = new Date(value)
+        return isNaN(dateValue) ? undefined : dataValue;
     },
     parseLiteral(ast) {
-        return (ast.kind == Kind.STRING) ? new Date(ast.value) : undefined
+        if (ast.kind == Kind.STRING) {
+            const value = new Date(ast.value);
+            return isNaN(value) ? undefined : value
+        }
     }
 });
 
@@ -46,6 +50,21 @@ const resolvers = {
     GraphQLDate,
 };
 
+// Book Error! Reference error! It's not validateIssue!
+// Book Error! Parameter is just issue, nothing more!
+function issueValidate(issue) {
+    const errors = []
+    if (issue.title.length < 3) {
+        errors.push('Do not be that lazy!')
+    }
+    if (issue.status == 'Assigned' && !issue.owner) {
+        errors.push('So to whom it is assigned then?!')
+    }
+    if (errors.length > 0) {
+        throw new UserInputError('Invalid input(s)', { errors })
+    }
+}
+
 function setAboutMessage(_, { message }) {
     return aboutMessage = message;
 }
@@ -55,9 +74,9 @@ function issueList() {
 }
 
 function issueAdd(_, { issue }) {
+    issueValidate(issue)
     issue.created = new Date()
     issue.id = issuesDB.length + 1
-    if (issue.status == undefined) issue.status = 'New'
     issuesDB.push(issue)
     return issue
 }
@@ -65,6 +84,10 @@ function issueAdd(_, { issue }) {
 const server = new ApolloServer({
     typeDefs: fs.readFileSync('./schema.graphql', 'utf-8'),
     resolvers,
+    formatError: error => {
+        console.log(error)
+        return error
+    }
 });
 
 await server.start(); // Added this line to make it work with @3 - Newest version
