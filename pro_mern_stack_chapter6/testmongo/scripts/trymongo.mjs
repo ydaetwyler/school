54f29beb6ce73aa8fdbfd26ecf08b1236a291664
ydaetwyler <ydaetwyler@previon.ch>
2021-09-10 22:05:18 +0200

    Test Success

---
+++ pro_mern_stack_chapter6/testmongo/scripts/trymongo.mjs
@@ -0,0 +1,54 @@
+import { MongoClient } from "mongodb";
+
+// %40 = @
+const url = 'mongodb+srv://filewalker:dxu7mbp1pqw_DZR%40enj@cluster0.fpmyd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+const testWithCallbacks = (callback) => {
+    console.log(`\n_-*>> testWithCallbacks <<*-_`)
+    const client = new MongoClient(url, { useNewUrlParser: true})
+
+    client.connect((err, client) => {
+        if (err) {
+            callback(err)
+            return
+        }
+
+        console.log(`🚀 Connected to MongoDB 🚀`)
+        
+        const db = client.db()
+        const collection = db.collection('issuetracker.employees')
+    
+        const employee = { id: 6, name: 'Dschingis Khan', age: 50 }
+    
+        collection.insertOne(employee, (err, result) => {
+            if (err) {
+                console.log(`Error -> Emergency Kill!`)
+                client.close()
+                callback(err)
+                return
+            }
+            
+            console.log(`Result ID of insert ->\n${result.insertedId}`)
+    
+            collection.find({ _id: result.insertedId})
+                .toArray((err, docs) => {
+                    if (err) {
+                        console.log(`Error -> Emergency Kill!`)
+                        callback(err)
+                        return
+                    }
+
+                    console.log(`Result of find ->\n${docs} `)
+
+                    console.log(`Job done - Time to die :(`)
+                    client.close()
+                })
+        })
+    })
+}
+
+testWithCallbacks((err) => {
+    err
+    ? console.log(err)
+    : console.log(`Tested and liked it :)`)
+})
