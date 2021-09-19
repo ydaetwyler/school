cd93a53cfc50f90b0da215cd571cf6f5bf6a598a
ydaetwyler <ydaetwyler@previon.ch>
2021-09-19 18:42:22 +0200

    server with apollo & express

--- projectx/backend/server/src/server.mjs
+++ projectx/backend/server/src/server.mjs
@@ -1,18 +1,46 @@
-import { ApolloServer } from 'apollo-server'
+import { ApolloServer } from 'apollo-server-express'
+import dotenv from 'dotenv'
+import express from 'express'
+import mongoose from 'mongoose'
 
 import typeDefs from './schema/schema.mjs'
-
 import connect from './utils/db.mjs'
 
 // Connect to MongoDB
 connect()
 
-const server = new ApolloServer({ typeDefs })
+dotenv.config()
 
-server.listen().then(() => {
+const app = express()
+
+const server = new ApolloServer({ 
+    typeDefs,
+    cors: true,
+    playground: process.env.NODE_ENV === 'development' ? true : false,
+    introspection: true,
+    tracing: true,
+    path: '/',
+})
+
+await server.start()
+
+server.applyMiddleware({
+    app,
+    path: '/',
+    cors: true,
+    onHealthCheck: () => {
+        new Promise((resolve, reject) => {
+            (mongoose.connection.readyState > 0)
+            ? resolve()
+            : reject()
+        })
+    }
+})
+
+app.listen({ port: process.env.PORT }, () => {
     console.log(`
         🚀  Server is running!
-        🔉  Listening on port 4000
+        🔉  Listening on port ${process.env.PORT}
         📭  Query at https://studio.apollographql.com/dev
     `)
 })
