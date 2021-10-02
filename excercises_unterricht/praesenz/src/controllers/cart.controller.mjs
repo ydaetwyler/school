90f6822a0ee721d9fb2bf5fb18d9a88856aa22fb
ydaetwyler <ydaetwyler@previon.ch>
2021-10-02 11:53:23 +0200

    refactoring

--- excercises_unterricht/praesenz/src/controllers/cart.controller.mjs
+++ excercises_unterricht/praesenz/src/controllers/cart.controller.mjs
@@ -1,28 +1,14 @@
 import CartItem from '../models/cartItem.mjs'
-import { getArticle } from '../services/article.service.mjs'
 
-import { getCartData } from '../services/cart.service.mjs'
+import { getArticle } from '../services/article.service.mjs'
+import { getCartData, addToCart } from '../services/cart.service.mjs'
 
 export const getCart = async (req,res,next) => res.send(await getCartData())
 
 export const addItem = async (req,res,next) => {
     const article = await getArticle(req.params.id)
-
-    const articleExists = await CartItem.findOne({ articleData: article._id })
-
-    if (articleExists) {
-        await CartItem.findOneAndUpdate(
-            { articleData: article._id },
-            { $inc: { quantity: 1 }, price: article.price },
-            { upsert: true }
-        )
-    } else {
-        await CartItem.create({
-            articleData: article._id,
-            price: article.price,
-            quantity: 1
-        })
-    }
+    
+    await addToCart(article)
     
     res.send(await getCartData())
 }
--- excercises_unterricht/praesenz/src/services/cart.service.mjs
+++ excercises_unterricht/praesenz/src/services/cart.service.mjs
@@ -1,5 +1,11 @@
 import CartItem from "../models/cartItem.mjs"
 
+const calculateCart = items => {
+    let amount = 0
+    items.forEach(item => (amount += item.price * 100 * item.quantity))
+    return parseFloat((amount / 100).toFixed(2))
+}
+
 export const getCartData = async () => {
     const cartItems = await CartItem.find().populate('articleData')
 
@@ -10,8 +16,20 @@ export const getCartData = async () => {
     }
 }
 
-const calculateCart = items => {
-    let amount = 0
-    items.forEach(item => (amount += item.price * 100 * item.quantity))
-    return parseFloat((amount / 100).toFixed(2))
+export const addToCart = async article => {
+    const articleExists = await CartItem.findOne({ articleData: article._id })
+
+    if (articleExists) {
+        return CartItem.findOneAndUpdate(
+            { articleData: article._id },
+            { $inc: { quantity: 1 }, price: article.price },
+            { upsert: true }
+        )
+    } else {
+        return CartItem.create({
+            articleData: article._id,
+            price: article.price,
+            quantity: 1
+        })
+    }
 }
