0f0bc54452a4ab8180fc948dcab1bf9b74cab3b3
ydaetwyler <ydaetwyler@previon.ch>
2021-12-04 10:52:34 +0100

    token problem not solved yet

--- projectx/frontend/project-x/src/components/Login/Login.js
+++ projectx/frontend/project-x/src/components/Login/Login.js
@@ -5,7 +5,7 @@ import UserAccess from './UserAccess'
 import NewUserAccess from './NewUserAccess'
 import NewFamily from './NewFamily'
 
-const Login = () => {
+const Login = ({ setToken }) => {
 
     return (
         <BrowserRouter>
@@ -13,9 +13,9 @@ const Login = () => {
             <Link to="/new">Create new Family</Link>
         </div>
             <Routes>
-                <Route exact path='/login/:hash' element={<NewUserAccess />} />
+                <Route exact path='/login/:hash' element={<NewUserAccess setToken={setToken} />} />
                 <Route path='/new' element={<NewFamily />} />
-                <Route path='/' element={<UserAccess />} />
+                <Route path='/' element={<UserAccess setToken={setToken} />} />
             </Routes>
         </BrowserRouter>
     )
--- projectx/frontend/project-x/src/components/Login/NewUserAccess.js
+++ projectx/frontend/project-x/src/components/Login/NewUserAccess.js
@@ -1,26 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const NewUserAccess = () => {
+import { gql, useMutation } from '@apollo/client'
+import { Navigate, useParams } from 'react-router-dom'
+
+const SIGN_UP = gql`
+    mutation SignUp($username: String!, $email: String!, $password: String!, $familyHash: String!) {
+        signUp(username: $username, email: $email, password: $password, familyHash: $familyHash)
+    }
+`
+
+const NewUserAccess = ({ setToken }) => {
+    const [familyHash, setFamilyHash] = useState(useParams().hash)
+    const [email, setEmail] = useState("")
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [signUp, { loading, error }] = useMutation(SIGN_UP, {
+        onCompleted: (data) => setToken(data.signUp)
+    })
+
+    const submitHandler = async event => {
+        event.preventDefault()
+        try {
+            await signUp({ variables: { username, email, password, familyHash } })
+        } catch(error) {
+            console.log(JSON.stringify(error, null, 2))
+        }
+    }
+
+    const emailInputHandler = event => setEmail(event.target.value)
+    const usernameInputHandler = event => setUsername(event.target.value)
+    const passwordInputHandler = event => setPassword(event.target.value)
 
     return (
         <div className="login-wrapper">
-            <h2>Invitation</h2>
-            <p>Create new user to join</p>
-            <form>
+            <h2>Create new user</h2>
+            <form onSubmit={submitHandler}>
                 <label>
                     <p>E-Mail</p>
-                    <input type="text" />
+                    <input onChange={emailInputHandler} type="text" />
                 </label>
                 <label>
                     <p>Username</p>
-                    <input type="text" />
+                    <input onChange={usernameInputHandler} type="text" />
                 </label>
                 <label>
                     <p>Password</p>
-                    <input type="password" />
+                    <input onChange={passwordInputHandler} type="password" />
                 </label>
                 <div>
-                    <button type="submit">Login</button>
+                    <button disabled={loading} type="submit">Login</button>
+                    {error && <p>{error.message}</p>}
                 </div>
             </form>
         </div>
