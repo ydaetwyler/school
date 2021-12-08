a92e8ab7f4e5dcddc38858b3fd8ffb981718b3d9
ydaetwyler <ydaetwyler@previon.ch>
2021-12-08 23:00:14 +0100

    corrected testing - use mockingprovider

--- projectx/frontend/project-x/src/components/Login/Login.test.js
+++ projectx/frontend/project-x/src/components/Login/Login.test.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import { render, screen } from "@testing-library/react"
 import userEvent from '@testing-library/user-event'
-import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
-import dotenv from 'dotenv'
-
+import { MockedProvider } from '@apollo/client/testing'
 
 import '@testing-library/jest-dom'
 
@@ -11,21 +9,13 @@ import Login from './Login'
 
 import { expect } from '@jest/globals'
 
-dotenv.config()
-const baseUrl = process.env.REACT_APP_BASE_URL
-
-const client = new ApolloClient({
-    uri: baseUrl,
-    cache: new InMemoryCache()
-})
-
 const leftClick = { button: 0 }
 
 test('Login rendering/navigating links', () => {
     render(
-        <ApolloProvider client={client}>
+        <MockedProvider>
             <Login />
-        </ApolloProvider>
+        </MockedProvider>
     )
 
     // verify to be on login page
