b0f2f43477bd4ca7904b32a3f425dd0a3ee8de10
ydaetwyler <ydaetwyler@previon.ch>
2022-01-02 21:37:56 +0100

    improved gallery

--- projectx/frontend/project-x/src/components/Activity/Gallery.js
+++ projectx/frontend/project-x/src/components/Activity/Gallery.js
@@ -20,7 +20,7 @@ const Gallery = ({ galleryClicked, setGalleryClicked, setImgUrl, imgUrl }) => {
     if (!galleryClicked) return null
 
     return (
-        <div className="h-screen w-full fixed top-0 bg-gray-600">
+        <div className="h-auto w-full absolute top-0 bg-gray-600">
             <img 
                 src="/icons/close.png" 
                 className="absolute top-1.5 right-1.5 h-6 w-6 opacity-30 hover:opacity-100 cursor-pointer"
@@ -28,7 +28,11 @@ const Gallery = ({ galleryClicked, setGalleryClicked, setImgUrl, imgUrl }) => {
             />
             <div className="flex flex-row flex-wrap mt-10 justify-around">
                 {activityImg.map((img) => 
-                    <img key={img} className={`h-32 w-40 mb-8 mx-4 cursor-pointer ${(img === imgUrl) ? "border-yellow-200 border-4" : "border-none"}`} src={img} onClick={() => setImgUrl(img)} />
+                    <img key={img} className={`h-40 w-60 mb-8 mx-4 cursor-pointer ${(img === imgUrl) ? "border-yellow-200 border-4" : "border-none"}`} src={img} onClick={() => {
+                            setImgUrl(img)
+                            setGalleryClicked(false)
+                        }
+                    } />
                 )}
             </div>
         </div>
